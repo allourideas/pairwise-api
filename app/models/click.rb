1fa52ab55625c7105cd96e96bb66f05664fd2223
Pius Uzamere <pius@uyiosa.com>
2009-12-07 16:45:24 -0500

    quick link to geocoded ip

--- app/models/click.rb
+++ app/models/click.rb
@@ -1,4 +1,21 @@
 class Click < ActiveRecord::Base
   belongs_to :site
   belongs_to :visitor
+  
+  def page
+    ip = what_was_clicked.split('[ip: ')[1].split(']').first rescue nil
+  end
+    
+  
+  def ip
+    ip = what_was_clicked.split('[ip: ')[1].split(']').first rescue nil
+  end
+  
+  def to_html
+    if ip
+      return what_was_clicked.gsub('ip', "http://api.hostip.info/get_html.php?ip=#{ip}")
+    else
+      return what_was_clicked
+    end
+  end
 end
