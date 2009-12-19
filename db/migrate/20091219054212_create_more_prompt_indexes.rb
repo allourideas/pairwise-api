b7ded5be5fac1ec080ea7522f2a47d44e897bd41
Pius Uzamere <pius@uyiosa.com>
2009-12-19 00:53:14 -0500

    giving the index a name

--- db/migrate/20091219054212_create_more_prompt_indexes.rb
+++ db/migrate/20091219054212_create_more_prompt_indexes.rb
@@ -1,6 +1,6 @@
 class CreateMorePromptIndexes < ActiveRecord::Migration
   def self.up
-    add_index :prompts, [:left_choice_id, :right_choice_id, :question_id], :name => "a_cool_index"
+    add_index :prompts, [:left_choice_id, :right_choice_id, :question_id], :name => "a_cool_index", :unique => true
   end
 
   def self.down
