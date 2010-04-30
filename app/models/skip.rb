a1fa6c617f950761662a21084b9c224324966e27
Dhruv Kapadia <git@dkapadia.com>
2010-04-30 17:22:14 -0400

    Adding associations to skip model

--- app/models/skip.rb
+++ app/models/skip.rb
@@ -1,4 +1,6 @@
 class Skip < ActiveRecord::Base
   belongs_to :skipper, :class_name => "Visitor", :foreign_key => "skipper_id"
-  belongs_to :prompt, :class_name => "Prompt", :foreign_key => "prompt_id"
+  belongs_to :question
+  belongs_to :prompt
+  belongs_to :appearance
 end
