142c9a765120b9acef23557d96fe43c970b70db0
Dhruv Kapadia <git@dkapadia.com>
2010-05-04 01:47:07 -0400

    Debugging delayed jobs issue in staging

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -51,5 +51,6 @@ Rails::Initializer.run do |config|
     :version => '1.5.1'
   config.gem 'delayed_job',
     :version => '2.0.3'
+  config.gem 'redis'
 end
 
