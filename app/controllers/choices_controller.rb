cd65ef1f12f77fa1aad66ce0f7b5fe8a33adac22
Dhruv Kapadia <git@dkapadia.com>
2010-05-10 11:10:21 -0400

    Adding information to support graph on AOI

--- app/controllers/choices_controller.rb
+++ app/controllers/choices_controller.rb
@@ -27,7 +27,7 @@ class ChoicesController < InheritedResources::Base
       end
     end
     index! do |format|
-      format.xml { render :xml => @choices.to_xml(:only => [ :data, :score, :id, :active], :methods => :user_created)}
+      format.xml { render :xml => @choices.to_xml(:only => [ :data, :score, :id, :active, :created_at, :wins, :losses], :methods => :user_created)}
     end
 
   end
