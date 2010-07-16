e3c668a38d18aaf04852386814028c038ecfa78e
Dhruv Kapadia <git@dkapadia.com>
2010-07-16 11:46:31 -0400

    Adding default hostname

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -15,3 +15,4 @@ ActionMailer::Base.smtp_settings = {
 DO_NOT_REPLY = "donotreply@example.com"
 CRON_EMAIL = ["cron@allourideas.org", 'aoi_cron@dkapadia.com', 'matt@allourideasorg']
 ERRORS_EMAIL = "errors@allourideas.org"
+HOST = "api.allourideas.org"
