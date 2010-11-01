81256140993571daef70ff19669d2105800fb465
Luke Baker <lukebaker@gmail.com>
2010-11-01 15:29:47 -0400

    update spacing in Appearance model

--- app/models/appearance.rb
+++ app/models/appearance.rb
@@ -1,12 +1,12 @@
 class Appearance < ActiveRecord::Base
-      belongs_to :voter, :class_name => "Visitor", :foreign_key => 'voter_id'
-      belongs_to :prompt
-      belongs_to :question
-      belongs_to :answerable, :polymorphic => true
+  belongs_to :voter, :class_name => "Visitor", :foreign_key => 'voter_id'
+  belongs_to :prompt
+  belongs_to :question
+  belongs_to :answerable, :polymorphic => true
 
-      default_scope :conditions => {:valid_record => true}
+  default_scope :conditions => {:valid_record => true}
 
-      def answered?
-	 !self.answerable_id.nil?
-      end
+  def answered?
+    !self.answerable_id.nil?
+  end
 end
