a1a41f50e164958c884397c47ee51de0da88c397
Luke Baker <lukebaker@gmail.com>
2011-04-01 09:33:23 -0400

    upgrade hoptoad to 2.4.9

--- config/environment.rb
+++ config/environment.rb
@@ -16,7 +16,7 @@ Rails::Initializer.run do |config|
   #config.action_mailer.delivery_method = :sendmail
 
   config.gem 'hoptoad_notifier',
-    :version => '2.3.4'
+    :version => '2.4.9'
   config.gem "ambethia-smtp-tls",
     :lib     => "smtp-tls",
     :version => "1.1.2",
