919f6e7993148c0fcf5da40e8965320997fc3305
Luke Baker <lukebaker@gmail.com>
2013-04-25 11:48:16 -0400

    add example of monkey patch usage

--- lib/rails23_ruby193_fixnum_monkey_patch.rb
+++ lib/rails23_ruby193_fixnum_monkey_patch.rb
@@ -1,6 +1,7 @@
 class Fixnum
   # actionpack-2.3.18/lib/action_controller/assertions/selector_assertions.rb:276
   # attempts to get encoding of Fixnum when doing has_tag "foo", :text => 3
+  # Example: bundle exec spec spec/integration/visitors_spec.rb -e "should return the bounces for a single question"
   def encoding
     Encoding::UTF_8
   end
