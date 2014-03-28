02998dcb89cfc525ea00f6cc79e42a02c37f755c
Luke Baker <lukebaker@gmail.com>
2014-03-28 10:57:59 -0400

    Revert "add String#force_encoding for Ruby 1.8"
    This reverts commit 7c4890d1ae94ea39229bed615b30d4313ebf8e01.

--- lib/force_encoding_ruby18.rb
+++
@@ -1,9 +0,0 @@
-# Ruby 1.8 String does not have force_encoding method
-# Add it to do nothing if it doesn't exist.
-if ! "".methods.include? :force_encoding
-class String
-  def force_encoding(enc)
-    self
-  end
-end
-end
