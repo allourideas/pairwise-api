419e6136eac5afe93b4e37ff9bb286fecd93aaaf
Luke Baker <lukebaker@gmail.com>
2014-04-17 10:37:59 -0400

    update delayed job monit group name

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,4 +1,4 @@
 #send hoptoad deploy notification
 run "cd #{config.release_path} && rake hoptoad:deploy TO=#{config.node[:environment][:framework_env]} USER=#{config.node[:owner_name]} REVISION=#{`cat #{config.release_path}/REVISION`}"
 #restart delayed jobs
-run "sudo monit restart all -g pairwise2_jobs"
+run "sudo monit restart all -g dj_pairwise2"
